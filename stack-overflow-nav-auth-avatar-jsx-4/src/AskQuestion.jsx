80bf7aedb569d44a08ac1d1b88d8e55b64e66d11
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-11-24 08:45:43 +0530

    Update AskQuestion.jsx

--- stack-overflow-nav-auth-avatar-jsx-4/src/AskQuestion.jsx
+++ stack-overflow-nav-auth-avatar-jsx-4/src/AskQuestion.jsx
@@ -4,9 +4,46 @@ const AskQuestion = () => {
   
  return <div>
     <>
-    
+    <div className="ask-ques">
+    <div className="ask-ques-container">
+      <h1>Ask a public Question</h1>
+      <form>
+        <div className="ask-form-container">
+          <label htmlFor = "ask-ques-title">
+            <h4>Title</h4>
+            <input type = "text" className = "questionTitle" id = "ask-ques-title" placeholder = "" />
+          </label>
+          </form>
+         </div> 
+        </div>
+      
+      <div className="ask-ques">
+    <div className="ask-ques-container">
+      <h1>Ask a public Question</h1>
+      <form>
+        <div className="ask-form-container">
+          <label htmlFor = "ask-ques-body">
+            <h4>Body</h4>
+            <input type = "text" className = "questionTitle" id = "ask-ques-title" placeholder = "" />
+          </label>
+
+          </form>
+          <div className="ask-ques">
+    <div className="ask-ques-container">
+      <h1>Ask a public Question</h1>
+      <form>
+        <div className="ask-form-container">
+          <label htmlFor = "ask-ques-tags">
+            <h4>tags</h4>
+            <input type = "text" className = "questionTitle" id = "ask-ques-title" placeholder = "" />
+          </label>
+          </form>
+          </div>
+          <input type = "Submit" value = "Review your question" className = "questionTitle" id = "ask-ques-title" placeholder = "" />
+    </div>
+    </div>
     </>
+
   </div>;
 };
 export default AskQuestion;
-
